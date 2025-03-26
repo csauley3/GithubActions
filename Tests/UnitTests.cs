f3fb510e29417e84accaa19f2f96361198c5dfbe
Collin Sauley <collinsauley@gmail.com>
2025-03-25 23:07:47 -0500

    added subtraction UnitTests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -27,3 +27,30 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+public class Subtraction
+{
+	[TestMethod]
+	public void Sub_Valid_Patino_SAULEY()
+	{
+		Assert.AreEqual(1, Program.Subtract("2", "1"));
+		Assert.AreEqual(10, Program.Subtract("20", "10"));
+		Assert.AreEqual(5, Program.Subtract("10", "5"));
+	}
+
+	[TestMethod]
+	public void Sub_Invalid_Patino_SAULEY()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+	}
+
+	[TestMethod]
+	public void Sub_Null_Patino_SAULEY()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+	}
+}
